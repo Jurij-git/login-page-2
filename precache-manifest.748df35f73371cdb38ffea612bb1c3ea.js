3b01a81e03c93f38501cf6a2ceea7518703fe8d5
Jurij-git <8good8@gmail.com>
2020-04-25 12:44:58 -0400

    Updates

---
+++ precache-manifest.748df35f73371cdb38ffea612bb1c3ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874f4ab74cdb1001b3827cb2aa7dcd4e",
+    "url": "/login-page-2/index.html"
+  },
+  {
+    "revision": "beb08be77c25aa9c1715",
+    "url": "/login-page-2/static/css/main.1e38c4a7.chunk.css"
+  },
+  {
+    "revision": "127114334207f0887714",
+    "url": "/login-page-2/static/js/2.8833f6be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/login-page-2/static/js/2.8833f6be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb08be77c25aa9c1715",
+    "url": "/login-page-2/static/js/main.bf1e2149.chunk.js"
+  },
+  {
+    "revision": "9b098143a49fb5944bcb",
+    "url": "/login-page-2/static/js/runtime-main.b7fc581d.js"
+  }
+]);
